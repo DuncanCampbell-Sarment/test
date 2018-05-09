10a6c914141e0ad1fae82b07fc3e89cefe733a3f
DuncanCampbell-Sarment <duncan.campbell@sarment.com>
2018-05-09 14:03:24 +0800

    simplified test

--- src/common.js
+++ src/common.js
@@ -1,16 +1,4 @@
-exports.generateAthenaKey = (data, keyPrefix, timestamp, id) => {
-
-  var eventDate = timestamp; //new Date(timestamp * 1000); //convert from unix timestamp
-  var year = eventDate.getUTCFullYear(); 
-  var month = eventDate.getUTCMonth() + 1; 
-  var day = eventDate.getUTCDate(); 
-
-  var key = keyPrefix + "year=" + year + "/month=" + month + "/day=" + day + "/" + id; 
-
-  data.year = year; 
-  data.month = month; 
-  data.day = day; 
-  data.iso_timestamp = eventDate.toISOString(); 
-
-  return key + ".json.gz"; 
+exports.getProcessPromises = (messages) => {
+  return [];
 };
+
--- src/common.test.js
+++ src/common.test.js
@@ -1,42 +1,8 @@
 'use strict';
 const common = require('./common');
 
-describe('generateAthenaKey', () => {
-  test('generates correctly formatted Athena key', () => {
-    let timestamp = new Date(2020, 2);
-    timestamp.setUTCDate(29);
-
-    expect(common.generateAthenaKey(
-      {
-        'year': '2017',
-        'month': '0',
-        'day': '31'
-      },
-      'key',
-      timestamp,
-      'id'
-    )).toBe('keyyear=2020/month=2/day=29/id.json.gz');
-  });
-
-  test('updates timestamp', () => {
-    let timestamp = new Date(2020, 2);
-    timestamp.setUTCDate(29);
-    let data = {
-      'year': '2017',
-      'month': '0',
-      'day': '31'
-    };
-
-    common.generateAthenaKey(
-      data,
-      'key',
-      timestamp,
-      'id'
-    );
-
-    expect(data.year).toBe(2020);
-    expect(data.month).toBe(2);
-    expect(data.day).toBe(29);
-    expect(data.iso_timestamp).toBe('2020-02-29T16:00:00.000Z');
-  });
-});
+describe('getProcessPromises', () => {
+  test('given [] returns []', () => {
+    expect(common.getProcessPromises([])).toEqual([]);
+  })
+})
